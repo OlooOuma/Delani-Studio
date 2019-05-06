f2d97badad6b3c997aedab4b4b85657cd9d9d5a4
Oloo Ouma <oloosolomon@gmail.com>
2019-05-06 09:26:15 +0300

    Add style to the tail class

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#showing").toggle();
-    $("#hidden").toggle();
-  });
+$(document).ready(function(){
+    $("#col").click(function(){
+        $(".showing").slideToggle();
+        $(".hidden").slideToggle();
+    });
+
+    $("#col").click(function(){
+        $(".showing").slideToggle();
+        $(".hidden").slideToggle();
+    });
+    $("#lg").click(function(){
+        $(".showing").slideToggle();
+        $(".hidden").slideToggle();
+    })
 });
